4d0d9bf0e33a798e7e3b9893f07e34fa010905b0
hyungkiShin <tkaqkeldk@nate.com>
2022-05-28 23:05:15 +0900

    initial Commit

---
+++ src/main/java/com/proj/covidgetline/CovidGetLineApplication.java
@@ -0,0 +1,13 @@
+package com.proj.covidgetline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidGetLineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovidGetLineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/proj/covidgetline/CovidGetLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj.covidgetline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidGetLineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
