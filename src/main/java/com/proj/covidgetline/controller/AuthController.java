69a3be94123fcf47b38d9f091d30e808653f09de
hyungkiShin <tkaqkeldk@nate.com>
2022-05-29 00:55:05 +0900

    feat controller add

--- src/main/java/com/proj/covidgetline/controller/AuthController.java
+++ src/main/java/com/proj/covidgetline/controller/AuthController.java
@@ -1,9 +1,18 @@
 package com.proj.covidgetline.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthController {
 
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
 
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup";
+    }
 }
