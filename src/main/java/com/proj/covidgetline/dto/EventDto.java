4d6f9bdfb36227028e0ab13a2dd5a5698f820913
hyungkiShin <tkaqkeldk@nate.com>
2022-06-01 16:07:26 +0900

    feat domain add

--- src/main/java/com/proj/covidgetline/dto/EventDto.java
+++ src/main/java/com/proj/covidgetline/dto/EventDto.java
@@ -1,89 +1,81 @@
 package com.proj.covidgetline.dto;
 
 import com.proj.covidgetline.constant.EventStatus;
+import com.proj.covidgetline.domain.Event;
+import com.proj.covidgetline.domain.Place;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
-public class EventDto (
-        Long id,
-        PlaceDto placeDto,
-        String eventName,
-        EventStatus eventStatus,
-        LocalDateTime eventStartDatetime,
-        LocalDateTime eventEndDatetime,
-        Integer currentNumberOfPeople,
-        Integer capacity,
-        String memo,
-        LocalDateTime createdAt,
-        LocalDateTime modifiedAt)
-        {
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class EventDto {
+    private Long id;
+    private PlaceDto placeDto;
+    private String eventName;
+    private EventStatus eventStatus;
+    private LocalDateTime eventStartDatetime;
+    private LocalDateTime eventEndDatetime;
+    private Integer currentNumberOfPeople;
+    private  Integer capacity;
+    private String memo;
+    private  LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
 
-public static EventDto of(
-        Long id,
-        PlaceDto placeDto,
-        String eventName,
-        EventStatus eventStatus,
-        LocalDateTime eventStartDatetime,
-        LocalDateTime eventEndDatetime,
-        Integer currentNumberOfPeople,
-        Integer capacity,
-        String memo,
-        LocalDateTime createdAt,
-        LocalDateTime modifiedAt
-        ){
+    public static EventDto of(Event event) {
         return new EventDto(
-        id,
-        placeDto,
-        eventName,
-        eventStatus,
-        eventStartDatetime,
-        eventEndDatetime,
-        currentNumberOfPeople,
-        capacity,
-        memo,
-        createdAt,
-        modifiedAt
+                event.getId(),
+                PlaceDto.of(event.getPlace()),
+                event.getEventName(),
+                event.getEventStatus(),
+                event.getEventStartDatetime(),
+                event.getEventEndDatetime(),
+                event.getCurrentNumberOfPeople(),
+                event.getCapacity(),
+                event.getMemo(),
+                event.getCreatedAt(),
+                event.getModifiedAt()
         );
-        }
+    }
 
-public static EventDto of(Event event){
-        return new EventDto(
-        event.getId(),
-        PlaceDto.of(event.getPlace()),
-        event.getEventName(),
-        event.getEventStatus(),
-        event.getEventStartDatetime(),
-        event.getEventEndDatetime(),
-        event.getCurrentNumberOfPeople(),
-        event.getCapacity(),
-        event.getMemo(),
-        event.getCreatedAt(),
-        event.getModifiedAt()
-        );
-        }
-
-public Event toEntity(Place place){
+    public Event toEntity(Place place) {
         return Event.of(
-        place,
-        eventName,
-        eventStatus,
-        eventStartDatetime,
-        eventEndDatetime,
-        currentNumberOfPeople,
-        capacity,
-        memo
+                place,
+                eventName,
+                eventStatus,
+                eventStartDatetime,
+                eventEndDatetime,
+                currentNumberOfPeople,
+                capacity,
+                memo
         );
-        }
+    }
 
-public Event updateEntity(Event event){
-        if(eventName!=null){event.setEventName(eventName);}
-        if(eventStatus!=null){event.setEventStatus(eventStatus);}
-        if(eventStartDatetime!=null){event.setEventStartDatetime(eventStartDatetime);}
-        if(eventEndDatetime!=null){event.setEventEndDatetime(eventEndDatetime);}
-        if(currentNumberOfPeople!=null){event.setCurrentNumberOfPeople(currentNumberOfPeople);}
-        if(capacity!=null){event.setCapacity(capacity);}
-        if(memo!=null){event.setMemo(memo);}
+    public Event updateEntity(Event event) {
+        if (eventName != null) {
+            event.setEventName(eventName);
+        }
+        if (eventStatus != null) {
+            event.setEventStatus(eventStatus);
+        }
+        if (eventStartDatetime != null) {
+            event.setEventStartDatetime(eventStartDatetime);
+        }
+        if (eventEndDatetime != null) {
+            event.setEventEndDatetime(eventEndDatetime);
+        }
+        if (currentNumberOfPeople != null) {
+            event.setCurrentNumberOfPeople(currentNumberOfPeople);
+        }
+        if (capacity != null) {
+            event.setCapacity(capacity);
+        }
+        if (memo != null) {
+            event.setMemo(memo);
+        }
 
         return event;
-        }
-        }
+    }
+}
--- src/main/java/com/proj/covidgetline/dto/PlaceDto.java
+++ src/main/java/com/proj/covidgetline/dto/PlaceDto.java
@@ -1,4 +1,86 @@
 package com.proj.covidgetline.dto;
 
+
+import com.proj.covidgetline.constant.PlaceType;
+import com.proj.covidgetline.domain.Place;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
 public class PlaceDto {
+
+    private Long id;
+    private PlaceType placeType;
+    private String placeName;
+    private String address;
+    private String phoneNumber;
+    private Integer capacity;
+    private String memo;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+
+    public static PlaceDto idOnly(Long id) {
+        return PlaceDto.of(id, null, null, null, null, null, null, null, null);
+    }
+
+    public static PlaceDto of(
+            Long id,
+            PlaceType placeType,
+            String placeName,
+            String address,
+            String phoneNumber,
+            Integer capacity,
+            String memo,
+            LocalDateTime createdAt,
+            LocalDateTime modifiedAt
+    ) {
+        return new PlaceDto(id, placeType, placeName, address, phoneNumber, capacity, memo, createdAt, modifiedAt);
+    }
+
+    public static PlaceDto of(Place place) {
+        return new PlaceDto(
+                place.getId(),
+                place.getPlaceType(),
+                place.getPlaceName(),
+                place.getAddress(),
+                place.getPhoneNumber(),
+                place.getCapacity(),
+                place.getMemo(),
+                place.getCreatedAt(),
+                place.getModifiedAt()
+        );
+    }
+
+    public Place toEntity() {
+        return Place.of(placeType, placeName, address, phoneNumber, capacity, memo);
+    }
+
+    public Place updateEntity(Place place) {
+        if (placeType != null) {
+            place.setPlaceType(placeType);
+        }
+        if (placeName != null) {
+            place.setPlaceName(placeName);
+        }
+        if (address != null) {
+            place.setAddress(address);
+        }
+        if (phoneNumber != null) {
+            place.setPhoneNumber(phoneNumber);
+        }
+        if (capacity != null) {
+            place.setCapacity(capacity);
+        }
+        if (memo != null) {
+            place.setMemo(memo);
+        }
+
+        return place;
+    }
+
 }
